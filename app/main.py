71e131cc3fc125ccdb2096a9f647e9b63844674d
Maciej Majewski <maciek12majewski@gmail.com>
2025-07-01 23:20:41 +0200

    fix flake8 tests

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str,
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int):
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -16,9 +26,13 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark >= self.clean_power:
             return 0.0
-        price = (car.comfort_class *
-                 (self.clean_power - car.clean_mark) *
-                 self.average_rating / self.distance_from_city_center)
+
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -31,6 +45,7 @@ class CarWashStation:
             if car.clean_mark < self.clean_power:
                 total_income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
+
         return round(total_income, 1)
 
     def rate_service(self, new_rate: int) -> None:
